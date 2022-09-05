3bcd18db7efc23e521557070b9534c0a04a249b9
jolugbo olufemi <jolugbofemi@gmail.com>
2022-09-05 02:43:19 +0100

    added a testConnection firebase fuctions to detect edurald backend connections)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,8 @@ import * as functions from "firebase-functions";
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const testConnection = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  console.log("connection test successful");
+  response.send("Welcome, you have reached the edurald engine.");
 });
