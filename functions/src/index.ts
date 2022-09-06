2c9154d9cd0ae1fb627f26b5a067b3c0151e3a48
jolugbo olufemi <jolugbofemi@gmail.com>
2022-09-06 14:32:56 +0100

    working on getting all careers at the backend)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -7,4 +9,17 @@ export const testConnection = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   console.log("connection test successful");
   response.send("Welcome, you have reached the edurald engine.");
+});
+
+export const getAllCareers = functions.https.onRequest((request, response) => {
+  admin.firestore().doc("careers").get()
+      .then((snapshot)=>{
+        console.log(snapshot.data);
+        response.send(snapshot.data);
+      })
+      .catch((error) =>{
+        console.log("error got caught");
+        response.send(error);
+      });
+  response.send("error fetching careers");
 });
